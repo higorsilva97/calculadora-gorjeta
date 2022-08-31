62e1be6846cd7a0110ef6ef967c8743ae094dd5e
Higor Silva <higorsilva97@gmail.com>
2022-08-31 11:18:48 -0300

    finalizando

--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
 let valorConta = document.getElementById("valorConta");
 let porcGorjeta = document.getElementById('porcGorjeta');
 let numPessoas = document.getElementById('numPessoas');
+let resultados = document.getElementById('resultados')
 
 function calcular()
 {   
     let valorGorjeta =  parseFloat(valorConta.value) * parseFloat(porcGorjeta.value) / 100;
     let totalPagar = parseFloat(valorConta.value) + valorGorjeta;
-    console.log(totalPagar);
+    gorjetasPorPessoas = valorGorjeta / numPessoas.value;
+    let totalPorPessoas = totalPagar / numPessoas.value;
+    if ((valorConta !== "" ) && (valorConta !== null) && (valorConta !== undefined))
+    {
+            let resultado = 
+            `<div class="resultado">
+                <h3 id="result">Resultado:</h3>
+                <h3>Gorjetas por pessoas: ${gorjetasPorPessoas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
+                <h3>Total por pessoas: ${totalPorPessoas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
+                <h3>Total a pagar: ${totalPagar.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} </h3>   
+            </div>`
+
+         resultados.innerHTML = resultado;  
+        }
+        
 }
 
 function limpar()
 {
-
+    resultados.innerHTML = "";
+    porcGorjeta.value = 0;
+    numPessoas.value = 1;
+    valorConta.value = 0;
+    resultados.focus();
 }
 
 
